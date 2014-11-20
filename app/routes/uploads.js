b3d4ee1e2a73b6ecd1a5e5bddd4e451fdbea1a64
stanley zheng <dev.stanzheng@gmail.com>
2014-11-19 22:27:47 -0500

    adding images but cant use streams

--- app/routes/uploads.js
+++ app/routes/uploads.js
@@ -7,6 +7,7 @@ var AWS = require('aws-sdk');
 var s3 = require('s3');
 var fs = require('fs');
 var config = require('../config/s3config.json');
+var s3Stream = require('s3-upload-stream')(new AWS.S3());
 /**
  * Don't hard-code your credentials!
  * Export the following environment variables instead:
@@ -28,12 +29,12 @@ AWS.config.update({
     //"region": "sa-east-1"   <- If you want send something to your bucket, you need take off this settings, because the S3 are global.
 });
 
-function upload(){
+function upload(myKey,myBody){
   // Create a bucket using bound parameters and put something in it.
   // Make sure to change the bucket name from "myBucket" to something unique.
   var s3bucket = new AWS.S3({params: {Bucket: 'stanzheng-staging'}});
   s3bucket.createBucket(function() {
-     var data = {Key: 'so this internet?'};
+     var data = {Key: myKey, Body: myBody };
     // s3bucket.getSignedUrl('getObject', data, function(err, url){
     //   console.log('the url of the image is', url);
     // });
@@ -49,13 +50,13 @@ function upload(){
 //...
 router.post('/', function(req, res) {
     var fstream;
-    upload();
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
         console.log("Uploading: " + filename);
         fstream = fs.createWriteStream(__dirname + '/files/' + filename);
         file.pipe(fstream);
         fstream.on('close', function () {
+            upload("image.jpg",fstream);
             res.redirect('back');
         });
     });

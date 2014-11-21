ea95ffc4a55884e57c1fc5803c808ad8d7f46968
stanley zheng <dev.stanzheng@gmail.com>
2014-11-21 09:24:15 -0500

    adding the time stream

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var clock = require('./clock.js');
+var moment = require('moment');
 
 /* GET home page. */
+console.log(moment().format());
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  //console.log(clock());
+  res.render('clock', { title: 'The Time is', image: clock(), time: moment().format("dddd, MMMM Do YYYY, h:mm:ss a") });
 });
 
 module.exports = router;
